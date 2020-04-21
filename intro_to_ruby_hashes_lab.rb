445aace15a4ea9f6393a2c9121a856effc8bb180
steven <bbtrombone@sbcglobal.net>
2020-04-21 02:52:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-   testing ={ :railroads =>{pieces: 4 ,:rent_in_dollars =>{one_piece_owned: "25",},:names=>{},} }
+   testing ={ :railroads =>{pieces: 4 ,:rent_in_dollars =>{one_piece_owned: 25 ,},:names=>{},} }
 
 end
 
